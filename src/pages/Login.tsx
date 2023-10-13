19810379212eaa5854ef5171b713b018b74d69ae
Tata Slyadneva <79031563664@ya.ru>
2023-10-13 03:06:02 +0300

    fix: Form imports

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Form from '../components/Form/Form';
+import { Form } from '../components/Form/Form';
 import { userData } from '../../types/userTypes';
 
 interface Props {
